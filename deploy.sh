e996c1810196637a60b7ce2ada8cc4d78fac5294
Maxime Charron <maksouw85@gmail.com>
2020-07-31 16:06:34 +0200

    fix: deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env sh
+
+set -e
+
 npm run build
 cd dist
-git add .
-git commit -m "deploy app"
-git push -f git@github.com:MaksOuw/darts-app master:gh-pages
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy app'
+
+git push -f git@github.com:MaksOuw/darts-app.git master:gh-pages
+
+cd -
